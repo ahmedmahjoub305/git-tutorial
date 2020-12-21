871921f5c0a851e22d9f96c6db941f58c76810a6
ahmedmahjoub305 <ahmedmahjoub305@gmail.com>
2020-12-21 08:09:07 +0800

    recycleview tutorial 1

--- app/src/main/java/mmu/edu/my/recycleview/MainActivity.java
+++ app/src/main/java/mmu/edu/my/recycleview/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
             holder.textView.setText(elements.get(position).getName());
             holder.textView2.setText(elements.get(position).getDescription());
             holder.imageView.setImageResource(elements.get(position).getIcon());
+
         }
 
         @Override
@@ -68,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
             notifyDataSetChanged();
         }
 
-        class MyViewHolder extends RecyclerView.ViewHolder {
+        class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
             public TextView textView;
             public TextView textView2;
             public ImageView imageView;
@@ -77,6 +79,15 @@ public class MainActivity extends AppCompatActivity {
                 textView = itemView.findViewById(R.id.versiontitle);
                 textView2 = itemView.findViewById(R.id.versionnumber);
                 imageView = itemView.findViewById(R.id.icon);
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                String name = elements.get(getAdapterPosition()).getName();
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
             }
         }
     }
